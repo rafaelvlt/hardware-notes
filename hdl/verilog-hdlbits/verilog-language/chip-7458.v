28960080ba3036ea1eed1d04e00844113e573327
rafaelvlt <rafaelmoonlight@hotmail.com>
2025-12-30 13:09:01 -0300

    feat: added hdl for the future

---
+++ hdl/verilog-hdlbits/verilog-language/chip-7458.v
@@ -0,0 +1,15 @@
+module top_module ( 
+    input p1a, p1b, p1c, p1d, p1e, p1f,
+    output p1y,
+    input p2a, p2b, p2c, p2d,
+    output p2y );
+	wire p1x, p1w, p2x, p2w;
+    assign p1x = p1a & p1b & p1c;
+    assign p1w = p1f & p1e & p1d;
+    assign p2x = p2a & p2b;
+    assign p2w = p2c & p2d;
+    assign p1y = p1x | p1w;
+    assign p2y = p2x | p2w;
+
+endmodule
+
